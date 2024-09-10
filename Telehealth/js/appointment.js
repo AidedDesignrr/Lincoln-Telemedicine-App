bdc5ace15adf2d7902a1d0fb68e0ea2fbc3c0dc7
AidedDesignrr <theokikijesuoladimeji@gmail.com>
2024-09-10 23:08:59 +0100

    Upload more files

---
+++ Telehealth/js/appointment.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+    const username = localStorage.getItem('username');
+    const departmentSelect = document.getElementById('departmentSelect');
+    const doctorSelect = document.getElementById('doctorSelect');
+    const appointmentForm = document.getElementById('appointmentForm');
+    const patientName =  document.getElementById('name');
+
+    if (username) {
+      // Set the value to the input field
+      document.getElementById('name').value = username;
+  }
+  
+    // Fetch and populate departments
+    fetch('/api/department')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(department => {
+          const option = document.createElement('option');
+          option.value = department._id;
+          option.textContent = department.name;
+          departmentSelect.appendChild(option);
+        });
+      })
+      .catch(error => console.error('Error fetching departments:', error));
+  
+    // Fetch and populate doctors based on selected department
+    departmentSelect.addEventListener('change', function () {
+      const departmentId = this.value;
+      doctorSelect.innerHTML = '<option value="">Select Doctors</option>'; // Clear previous options
+  
+      if (departmentId) {
+        fetch(`/api/doctor?department=${departmentId}`)
+          .then(response => response.json())
+          .then(data => {
+            data.forEach(doctor => {
+              const option = document.createElement('option');
+              option.value = doctor._id;
+              option.textContent = `${doctor.username} - ${doctor.specialization}`;
+              doctorSelect.appendChild(option);
+            });
+          })
+          .catch(error => console.error('Error fetching doctors:', error));
+      }
+    });
+
+    // Handle form submission
+    appointmentForm.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevent default form submission
+
+      const formData = new FormData(appointmentForm);
+      const jsonData = {};
+      formData.forEach((value, key) => jsonData[key] = value);
+
+      const token = localStorage.getItem('token');
+      
+
+      fetch('/api/appointment/book', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `${token}`, // Include the token in the Authorization header
+          },
+          body: JSON.stringify(jsonData),
+      })
+      .then(response => response.json())
+      .then(data => {
+          // Redirect to confirmation page or display a success message
+          window.location.href = '/confirmation';
+      })
+      .catch(error => console.error('Error creating appointment:', error));
+  });
+})
---
+++ Telehealth/js/loginLogout.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    const role = localStorage.getItem('role')
+    
+    const loginButton = document.getElementById('loginButton');
+    const signupButton = document.getElementById('signupButton');
+    const logoutButton = document.getElementById('logoutButton');
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    const dashboardButton = document.getElementById('dashboardButton');
+    
+    if (token && username) {
+      // User is logged in
+      loginButton.style.display = 'none';
+      signupButton.style.display = 'none';
+      logoutButton.style.display = 'inline';
+      welcomeMessage.appendChild = ` ${username}`;
+      dashboardButton.style.display = 'inline';
+    } else {
+      // User is not logged in
+      loginButton.style.display = 'inline';
+      signupButton.style.display = 'inline';
+      logoutButton.style.display = 'none';
+    }
+  
+    logoutButton.addEventListener('click', function() {
+      localStorage.removeItem('token');
+      alert('You have been logged out.');
+      // Optionally, you can also redirect to the home page or login page
+      window.location.href = '/';
+    });
+  
+    loginButton.addEventListener('click', function() {
+      window.location.href = '/login';
+    });
+  
+    signupButton.addEventListener('click', function() {
+      window.location.href = '/signup';
+    });
+
+    dashboardButton.addEventListener('click', function() {
+      if (role == 'user') window.location.href = '/patient';
+      else { window.location.href = '/doctor-dashboard';}
+    });
+  });
+  
